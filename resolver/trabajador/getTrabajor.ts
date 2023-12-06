0a834ac49e2bf3c24089d99c87c2e0c639c4453c
FranciscoJose03 <frodriguezc2@alumnos.nebrija.es>
2023-12-06 18:23:35 +0100

    update

--- resolver/trabajador/getTrabajor.ts
+++ resolver/trabajador/getTrabajor.ts
@@ -3,7 +3,7 @@ import { trabajadorModel, trabajadorModelType } from "../../db/dbTrabajador.ts";
 
 export const getTrabajador = async(req: Request<{id: string}>, res: Response<trabajadorModelType | {error: unknown} >) => {
     try{
-        const id = req.params;
+        const id = req.params.id;
         const trabajador = await trabajadorModel.findById(id);
         if(!trabajador){
             res.status(404).send({error: "Trabajador not found"});
